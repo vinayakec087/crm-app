c5dc12fef5ac0e5d5865ef3fec8529160e63d3f2
Vinayak Sammasgi <vinayakec087@gmail.com>
2023-03-19 17:20:38 +0530

    Lead Feature
    * Pull lead feature si done and tested.

---
+++ controller/lead.py
@@ -0,0 +1,18 @@
+# Lead controller implementation.
+
+
+# Create lead.
+def create_lead():
+  print 'Lead creation done.'
+
+# Update lead.
+def update_lead():
+  print 'Lead update done.'
+
+# Get lead.
+def get_lead():
+  print 'Get lead done.'
+
+# Delete lead.
+def delete_lead():
+  print 'Delete lead done.'
---
+++ model/lead.py
@@ -0,0 +1,19 @@
+
+# Lead model implementation.
+
+
+# Insert lead query.
+def insert_lead_query():
+  print 'Lead inserted into database.'
+
+# Update lead query.
+def update_lead_query():
+  print 'Lead updated into database.'
+
+# Get lead query.
+def select_lead_query():
+  print 'Lead select from database.'
+
+# Delete lead query.
+def delete_lead_query():
+  print 'Lead deleted from database.'
