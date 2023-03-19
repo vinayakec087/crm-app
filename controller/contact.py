6cefa591529d20b8bface10ade3a3b33d00a73cf
Vinayak Sammasgi <vinayakec087@gmail.com>
2023-03-19 18:49:01 +0530

    contact featurte in progress
    * Added get and remove conact functions

--- controller/contact.py
+++ controller/contact.py
@@ -5,6 +5,15 @@
 def create_contact():
   print 'Contact creation done.'
 
-# Update contact.
-def update_contact():
-  print 'Contact update done.
+# Edit contact.
+def edit_contact():
+  print 'Contact edit done.'
+
+  # get contact
+
+  def get_contact()
+    print 'conatct get done.'
+
+    # delete contact.
+def delete_contact():
+  print 'Contact delete done.'
